5d1a7bd09af13124b6053bec3dc84af5a90ff38d
Nicolas Hermosilla <nicolas.hermosilla@ynov.com>
2020-04-23 12:07:02 +0200

    sender.py update

--- arduino/sensor/sensor.py
+++ arduino/sensor/sensor.py
@@ -1,8 +1,9 @@
 import os
+import random
 
 sensor_array=["humidity","temperature","brightness","wind","altitude","atmospheric_pressure"]
 
 for i in range(0,len(sensor_array)):
     file = os.open(sensor_array[i],os.O_RDWR)
-    os.write(file,b"Hello")
+    os.write(file,random.randint(0,1024).to_bytes(2,"big"))
     os.close(file)
