2463bdb4448a69166f23cb12e6b43e8e7f4d3aca
Vincent G <vincent.gougeon@ynov.com>
2020-04-27 13:15:31 +0200

    Arduino C (only humidity)

--- arduino/arduino.c
+++ arduino/arduino.c
@@ -1,6 +1,43 @@
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#define MAX_BUF 1024
 
-int main() {
-	printf("Hello World");
+void readValue(char* path, char* value) {
+	int fd;
+	fd = open(path, O_RDWR);
+	read(fd, value, MAX_BUF);
+	close(fd);
+}
+
+void sendToPipe(char* path, char* value) {
+	int pipe;
+	pipe = open(path, O_RDWR);
+	write(pipe, value, 4);
+	close(pipe);
+}
+int main()
+{
+	int fd;
+	char * humidity = "sensor/humidity";
+	char * altitude = "sensor/altitude";
+	char * temperature = "sensor/temperature";
+	char * brightness = "sensor/brightness";
+	char * wind = "sensor/wind";
+	char * atmospheric_pressure = "sensor/atmospheric_pressure";
+	char buf[MAX_BUF];
+	int stop = 0;
+	while(stop == 0){
+		char value[MAX_BUF];
+		readValue(humidity, value);
+		printf("Value : %s\n", value);
+		sendToPipe("humidity", value);
+    		//write(fd, value, sizeof(value));
+		sleep(2);
+	}
+
 	return 0;
 }
