477efd5f0a3aa6c7e5445d9ed01aabec40669b56
thomas <hv@gmil.com>
2020-04-27 10:04:08 +0200

    SENSOR C EVERY SECOND

--- arduino/sensor/sensor.py
+++ arduino/sensor/sensor.py
@@ -1,9 +1,11 @@
 import os
 import random
-
+import time
 sensor_array=["humidity","temperature","brightness","wind","altitude","atmospheric_pressure"]
 
-for i in range(0,len(sensor_array)):
-    file = os.open(sensor_array[i],os.O_RDWR)
-    os.write(file,str.encode(str(random.randint(0,1024))))
-    os.close(file)
+while True:
+    for i in range(0,len(sensor_array)):
+        file = os.open(sensor_array[i],os.O_RDWR)
+        os.write(file,str.encode(str(random.randint(0,1024))))
+        os.close(file)
+    time.sleep(1)
