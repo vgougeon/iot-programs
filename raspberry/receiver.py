8b8df09239f17a17f6418cdc7768dc640637911a
Vincent G <vincent.gougeon@ynov.com>
2020-04-27 11:05:22 +0200

    Receiver.py

--- raspberry/receiver.py
+++ raspberry/receiver.py
@@ -4,18 +4,20 @@ from threading import Thread
 sensors=["humidity","temperature","brightness","wind","altitude","atmospheric_pressure"]
 
 class Sensor(Thread):
-  def __init__(cls, name):
+  def __init__(cls, name, sensorName):
     Thread.__init__(cls)
     cls.name = name
+    cls.sensorName = sensorName
   def run(cls):
     fifo = open(cls.name, "r")
     while True:
         for line in fifo:
+            #id = select id from sensor where name = cls.sensorName
+            #insert into data values (sensorId = id, value = line)
             print(cls.name + " value : " + line)
 
 threads = []
 
 for i in range(0, len(sensors)):
-    threads.append(Sensor("..arduino/sensor/" + sensors[i]))
+    threads.append(Sensor("../arduino/sensor/" + sensors[i], sensors[i]))
     threads[i].start()
-    threads[i].join()
